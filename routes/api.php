0e30d21f7a3fbe97b16bbf4b095469ea3586c9ec
AntoineDrsl <antoine.durussel@gmail.com>
2021-11-09 14:26:19 +0100

    :sparkles: FEAT crypto api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', function (Request $request) {
+    return Http::get('https://api.binance.com/api/v3/ticker/price');
+})->name('crypto');
